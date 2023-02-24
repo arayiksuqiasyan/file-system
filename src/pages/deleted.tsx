913c42f39d25d47d92fedbd8c8f29702d4a88a45
Arayik <arayiksuqiasyan20@gmail.com>
2023-02-24 20:08:49 +0400

    Updates

--- src/pages/deleted.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Deleted from "../componet/Deleted";
-
-const DeletedPage = () => {
-    return <Deleted/>
-};
-
-export default DeletedPage
--- src/pages/index.ts
+++
@@ -1,6 +0,0 @@
-import {lazy} from "react";
-
-export const HomePage = lazy(()=>import("./home"))
-export const SubPage = lazy(()=>import("./sub"))
-export const TextPage = lazy(()=>import("./text"))
-export const BasketPage = lazy(()=>import("./deleted"))
